d1d591560fb4a9a3d7c711c0150133338274be81
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 15:15:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,6 @@ def long_planeteer_calls(arr)
 end
 
 def find_the_cheese(s)
-  
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   s.find{|c| cheese_types.include?(c)}
 end
